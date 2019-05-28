b7118990ca71dd2646b300a5218d94bbfbc9a78a
Igor Bueno <igor_mcb@hotmail.com>
2019-05-27 22:17:52 -0400

    Novas alterações no reconhecimento

--- exercicio.py
+++ exercicio.py
@@ -14,45 +14,35 @@ pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tessera
 
 imagens = ['1.png']
 
+def marcar(small, rgb):
+        #threshold the image
+        _, bw = cv2.threshold(small, 0.0, 255.0, cv2.THRESH_BINARY_INV | cv2.THRESH_OTSU)
+        bw = cv2.dilate(bw, None, iterations=4)
+        # get horizontal mask of large size since text are horizontal components
+        kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (20, 1))
+        connected = cv2.morphologyEx(bw, cv2.MORPH_CLOSE, kernel)
+
+        # find all the contours
+        _, contours, hierarchy,=cv2.findContours(connected.copy(),cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
+        
+        #Segment the text lines
+        for idx in range(len(contours)):
+            x, y, w, h = cv2.boundingRect(contours[idx])            
+
+            cv2.rectangle(rgb, (x, y), (x+w-1, y+h-1), (0, 255, 0), 2)
+            imagemLeitura = rgb[y:(y + h), x:(w + x)]
+            # print(pytesseract.image_to_string(imagemLeitura))
+            plt.imshow(imagemLeitura)
+            plt.show()
+
 for img_ref in imagens:
-    img = cv2.imread(img_ref)
-    imagemCinza = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-    imagemCinza = cv2.blur(imagemCinza, (13,13))
+    # Executando tudo
+    rgb = cv2.imread(img_ref)
+    small = cv2.cvtColor(rgb, cv2.COLOR_BGR2GRAY)
 
-    imagemCinza = cv2.adaptiveThreshold(imagemCinza,255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 11, 2)
+    marcar(small, rgb)
 
-    imagemCinza = cv2.bitwise_not(imagemCinza)
-
-    imagemCinza = cv2.dilate(imagemCinza, None, iterations=5)
-    th3 = cv2.erode(imagemCinza, None, iterations = 5)
-
-    imagemCinza, contours, hierarchy = cv2.findContours(imagemCinza, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-    # newContours = sorted(contours, key=lambda x: cv2.contourArea(x))
-
-    areaImagem = imagemCinza.shape[0] * imagemCinza.shape[1]
-
-    for ctn in contours:
-        x, y, w, h = cv2.boundingRect(ctn)
-        areaContorno = cv2.contourArea(ctn)
-
-        proporcional = (100*areaContorno) / areaImagem
-
-        if(proporcional > 50 or proporcional < 0.5):
-            continue
-
-        cv2.rectangle(img, (x, y), (x + w, y + h), (255, 0, 0), 2)
-
-        imagemLeitura = img[y:h,x:w]
-
-        if not (imagemLeitura.shape[0] and imagemLeitura.shape[1]):
-            continue
-
-        plt.imshow(imagemLeitura)
-        plt.show()
-        print(pytesseract.image_to_string(imagemLeitura))
-
-    
-    # print(pytesseract.image_to_string(th3))
-    plt.imshow(img)
+    plt.imshow(rgb)
     plt.show()
+        
 
